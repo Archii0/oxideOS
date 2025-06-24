226d004e61f22ef24add59eed279126a58cae23e
Archii0 <archiedascombe@icloud.com>
2025-06-24 23:03:13 +0100

    Implement custom test framework

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,32 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+#![reexport_test_harness_main = "test_main"]
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
-    panic!("SOME PANIC");
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
 
+#![feature(custom_test_frameworks)]
+#![test_number(create::test_runner)]
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
